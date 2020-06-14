41ff41e31b6af7bb57e01624a6a847bccc875473
jsa275 <jsa275@sfu.ca>
2020-06-14 00:12:55 -0700

    basics of draggable

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,128 +1,176 @@
 <template>
-  <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
-    <main>
-      <div class="left-side">
-        <span class="title">
-          Welcome to your new project!
-        </span>
-        <system-information></system-information>
-      </div>
+	<div id="wrapper">
+		<div>Hello</div>
+		<el-row class='containerRow'>
+			<draggable
+				v-model="rooms"
+				class='sortable-list'
+				style='background:grey;'>
+				<el-col :span="6"
+					class='roomColumn sortable'
+					v-for="room in rooms"
+					:key="room.id">
+					<el-row class='roomHeader'>
+						name: {{ room.name }}
+					</el-row>
 
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
-      </div>
-    </main>
-  </div>
+					<draggable
+						group='room'
+						:list='room.devices'>
+						<el-row
+							class='deviceRow'
+							v-for='device in room.devices'
+							:key='device.id'>
+							deviceName: {{ device.name }}
+
+						</el-row>
+
+					</draggable>
+				</el-col>
+			</draggable>
+		</el-row>
+	</div>
 </template>
 
 <script>
-  import SystemInformation from './LandingPage/SystemInformation'
-
-  export default {
-    name: 'landing-page',
-    components: { SystemInformation },
-    methods: {
-      open (link) {
-        this.$electron.shell.openExternal(link)
-      }
-    }
-  }
+	import draggable from 'vuedraggable'
+	export default {
+		name: 'landing-page',
+		components: {  
+			draggable
+		},
+		data () {
+			return {
+				value1: true,
+				value2: true,
+				rooms: [
+					{
+						id:'1',
+						name:'room1',
+						devices:[
+							{
+								id:'1_device1',
+								name:'1_device1_name'
+							},
+							{
+								id:'1_device2',
+								name:'1_device2_name'
+							}
+						],
+					},
+					{
+						id:'2',
+						name:'room2',
+						devices:[
+							{
+								id:'2_device1',
+								name:'2_device1_name'
+							},
+							{
+								id:'2_device2',
+								name:'2_device2_name'
+							},
+						],
+					},
+					{
+						id:'3',
+						name:'room3',
+						devices:[
+							{
+								id:'3_device1',
+								name:'3_device1_name'
+							},
+							{
+								id:'3_device2',
+								name:'3_device2_name'
+							},
+							{
+								id:'3_device3',
+								name:'3_device3_name'
+							}
+						]
+					}
+				]
+			}
+		},
+		methods: {
+			open (link) {
+				this.$electron.shell.openExternal(link)
+			}
+		}
+	}
 </script>
 
-<style>
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+<style lang='scss'>
+	@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
 
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+	* {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
 
-  body { font-family: 'Source Sans Pro', sans-serif; }
+	body { font-family: 'Source Sans Pro', sans-serif; }
 
-  #wrapper {
-    background:
-      radial-gradient(
-        ellipse at top left,
-        rgba(255, 255, 255, 1) 40%,
-        rgba(229, 229, 229, .9) 100%
-      );
-    height: 100vh;
-    padding: 60px 80px;
-    width: 100vw;
-  }
+	#wrapper {
+		background:
+		radial-gradient(
+			ellipse at top left,
+			rgba(255, 255, 255, 1) 40%,
+			rgba(229, 229, 229, .9) 100%
+		);
+		height: 100vh;
+		padding: 60px 80px;
+		width: 100vw;
+	}
 
-  #logo {
-    height: auto;
-    margin-bottom: 20px;
-    width: 420px;
-  }
+	#logo {
+		height: auto;
+		margin-bottom: 20px;
+		width: 420px;
+	}
 
-  main {
-    display: flex;
-    justify-content: space-between;
-  }
+	main {
+		display: flex;
+		justify-content: space-between;
+	}
 
-  main > div { flex-basis: 50%; }
+	main > div { flex-basis: 50%; }
 
-  .left-side {
-    display: flex;
-    flex-direction: column;
-  }
+	.left-side {
+		display: flex;
+		flex-direction: column;
+	}
 
-  .welcome {
-    color: #555;
-    font-size: 23px;
-    margin-bottom: 10px;
-  }
+	.welcome {
+		color: #555;
+		font-size: 23px;
+		margin-bottom: 10px;
+	}
 
-  .title {
-    color: #2c3e50;
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 6px;
-  }
+	.title {
+		color: #2c3e50;
+		font-size: 20px;
+		font-weight: bold;
+		margin-bottom: 6px;
+	}
+	.el-col {
+		border-radius: 4px;
+	}
+	.roomColumn {
+		padding:10px;
+		margin:10px;
+		min-height: 500px;
+		background-color:gray;
+		/* background-color: red; */
+		.roomHeader {
+			background-color: cornsilk;
+		}
 
-  .title.alt {
-    font-size: 18px;
-    margin-bottom: 10px;
-  }
-
-  .doc p {
-    color: black;
-    margin-bottom: 10px;
-  }
-
-  .doc button {
-    font-size: .8em;
-    cursor: pointer;
-    outline: none;
-    padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
-    color: #fff;
-    background-color: #4fc08d;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #4fc08d;
-  }
-
-  .doc button.alt {
-    color: #42b983;
-    background-color: transparent;
-  }
+		.deviceRow {
+			padding: 5px;
+			margin: 5px;
+			background-color: rgb(240, 239, 239)
+		}
+	}
 </style>
